aa6ca477a4a33d71c8432249cf7f766dacb77125
DHINGLI DHINGLO <solankimimoh@gmail.com>
2018-03-30 23:38:35 +0530

    first commit

---
+++ app/src/main/java/com/example/rctechnical/rctechnical/AppConstant.java
@@ -0,0 +1,22 @@
+package com.example.rctechnical.rctechnical;
+
+/**
+ * Created by solan on 14-03-18.
+ */
+
+public class AppConstant {
+
+
+    //    DATABASE
+    final static String FIREBASE_TABLE_STUDNET = "students";
+    final static String FIREBASE_TABLE_FACULTY = "faculties";
+    final static String FIREBASE_DEPARTMENT = "department";
+    final static String FIREBASE_TABLE_FULLNAME = "fullname";
+    final static String FIREBASE_TABLE_EMAIL = "email";
+    final static String FIREBASE_TABLE_ENROLLMENT = "enrollment";
+    final static String FIREBASE_TABLE_MOBILE = "mobile";
+    final static String FIREBASE_DB_ROOT = "ljcollege-f7620";
+    final static String FIREBASE_DB_ISACTIVATED = "isActivated";
+
+
+}
